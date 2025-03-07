e42735cafa8934e149e17014777fc84c97cd73cd
Puyuan Yao <williamyao034@gmail.com>
2025-03-06 20:58:14 -0800

    update benchmark
    Signed-off-by: Puyuan Yao <williamyao034@gmail.com>

--- python/ray/dag/tests/experimental/allreduce_tuple_benchmark/plain_allreduces.py
+++ python/ray/dag/tests/experimental/allreduce_tuple_benchmark/plain_allreduces.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import time
 
 import ray
@@ -7,6 +8,8 @@ from ray.air._internal import torch_utils
 
 import torch
 
+import matplotlib.pyplot as plt
+
 
 ray.init(num_gpus=2)
 
@@ -14,71 +17,90 @@ torch.manual_seed(42)
 
 @ray.remote
 class TensorGenerator:
-    def __init__(self, tensor_size):
+    def __init__(self, tensor_size, n_tensors):
         self.device = torch_utils.get_devices()[0]
         self.tensor_size = tensor_size
+        # initialize a pool of tensors so that time is not spent on initialization during dag execution
+        self.tensors = set(torch.randn(self.tensor_size).to(self.device) for _ in range(n_tensors))
 
-    def generate(self, *args):
-        return torch.randn(self.tensor_size).to(self.device)
+    def get_tensor(self, *args):
+        # pop a tensor from the pool
+        return self.tensors.pop()
     
-    def generate_n(self, n):
-        return tuple(torch.randn(self.tensor_size).to(self.device) for _ in range(n))
+    def get_tuple(self, n):
+        # pop n tensors from the pool
+        return tuple(self.tensors.pop() for _ in range(n))
+    
+    # recv and refill tensors that have been popped
+    def recv(self, inp):
+        lst = [inp] if isinstance(inp, torch.Tensor) else list(inp)
+        for tensor in lst:
+            self.tensors.add(tensor)
+        return len(lst)
 
+# number of actors
 num_generators = 2
-num_tensors = [10, 15, 20, 25, 30]
-generators = [TensorGenerator.options(num_gpus=1).remote((4096, 8)) for _ in range(num_generators)]
+# number of tensors to allreduce
+num_tensors_lst = [32, 64, 128]
+# bucket sizes
+bucket_sizes = [1, 2, 4, 8, 16, 32]
+# shape of each tensor
+tensor_shape = (4096, 32)
+
+# two actors, one for each GPU
+generators = [TensorGenerator.options(num_gpus=1).remote(tensor_shape, 50) for _ in range(num_generators)]
+
+times = defaultdict(list)
 
 
-USE_TUPLE = False
-
-if USE_TUPLE:
-    with InputNode() as inp:
-        tensors = [generator.generate_n.bind(inp) for generator in generators]
-        results = allreduce.bind(tensors)
-        dag_tuple = MultiOutputNode(results)
-    
-    compiled_dag = dag_tuple.experimental_compile()
-    times = {}
-    for num_tensor in num_tensors:
-        # warm up
-        ref = compiled_dag.execute(num_tensor)
-        ray.get(ref)
-
-        time_total = 0
-        for i in range(5):
-            time_start = time.perf_counter()
-            ref = compiled_dag.execute(num_tensor)
-            ray.get(ref)
-            time_end = time.perf_counter()
-            time_total += time_end - time_start
-        times[num_tensor] = round(time_total / 5, 4)
-    for key, value in times.items():
-        print(f"Num generations: {key}, Avg time: {value} s")
-else:
-    times = {}
-    for num_tensor in num_tensors:
+for num_tensors in num_tensors_lst:
+    for bucket_size in bucket_sizes:
+        # number of allreduce operations
+        num_ar = int(num_tensors / bucket_size)
 
         with InputNode() as inp:
-            all_results = []
-            
-            for _ in range(num_tensor):
-                tensors = [generator.generate.bind(inp) for generator in generators]
-                all_results.extend(allreduce.bind(tensors))
+            inp0, inp1 = inp, inp
+            for _ in range(num_ar):
+                # if bucket_size is 1, use get_tensor, else use get_tuple
+                if bucket_size == 1:
+                    tensors = [
+                        generator.get_tensor.bind(_inp)
+                        for generator, _inp in zip(generators, [inp0, inp1])
+                    ]
+                else:
+                    tensors = [
+                        generator.get_tuple.bind(_inp)
+                        for generator, _inp in zip(generators, [inp0, inp1])
+                    ]
+                results = allreduce.bind(tensors)
+                inp0, inp1 = [
+                    generator.recv.bind(result)
+                    for generator, result in zip(generators, results)
+                ]
 
-            dag_no_tuple = MultiOutputNode(all_results)
-        
-        compiled_dag = dag_no_tuple.experimental_compile()
-        ref = compiled_dag.execute(None)
+            dag = MultiOutputNode([inp0, inp1])
+
+        # first time of running compiled dag is not counted
+        compiled_dag = dag.experimental_compile()
+        ref = compiled_dag.execute(bucket_size)
         ray.get(ref)
 
+        # average time of 5 runs
         time_total = 0
         for i in range(5):
             time_start = time.perf_counter()
-            ref = compiled_dag.execute(None)
+            ref = compiled_dag.execute(bucket_size)
             ray.get(ref)
             time_end = time.perf_counter()
-            time_total += time_end - time_start
-        times[num_tensor] = round(time_total / 5, 4)
+            time_total += (time_end - time_start)
+        times[num_tensors].append(time_total / 5 * 1000)
+        
         compiled_dag.teardown()
-    for key, value in times.items():
-        print(f"Num generations: {key}, Avg time: {value} s")
+
+for num_tensors in times.keys():
+    plt.plot(bucket_sizes, times[num_tensors], label=f"{num_tensors} tensors")
+plt.legend()
+plt.xlabel("Bucket size")
+plt.ylabel("Time (ms)")
+plt.title(f"tensor_size={tensor_shape}")
+plt.savefig("python/ray/dag/tests/experimental/allreduce_tuple_benchmark/allreduce_tuple_benchmark.png")
